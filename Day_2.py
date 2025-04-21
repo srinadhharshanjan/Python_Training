db0ab0055814f97db2f281513757a9a1349766c7
srinadhharshanjan <kvsssh97@gmail.com>
2025-04-21 18:13:41 +0530

    Initial commit

---
+++ Day_2.py
@@ -0,0 +1,40 @@
+# a = input("write a number: ")
+# b = input("write a number: ")   
+# c = int(a) + int(b)
+# print(f"The Numbers given are first number - {a},\nThe Second Number- {b} \nThe sum of the two numbers is:{c}")
+# print(c) 
+
+
+
+a = -3
+b = "4"
+c = True
+d = -4.5
+print(type(a))
+print(type(b))  
+print(type(c))
+print(type(d))
+
+
+a = input("write a number: ")
+b = input("write a number: ")
+print("the number is :",a,b)
+print(f"the first number a is {a}, \nand the second number b is {b}")
+
+
+x = input ("Enter a number: ")
+y = input ("Enter a number: ")
+
+addition = int(x) + int(y)
+subtraction = int(x) - int(y)  
+multiplication = int(x) * int(y)
+division = int(x) / int(y)
+modulus = int(x) % int(y)
+exponent = int(x) ** int(y)
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+print(f"Modulus: {modulus}")
+print(f"Exponent: {exponent}")
